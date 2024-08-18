759954bf1cc521bcd15744e8fa478084a4fcdf76
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-08-18 18:39:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import React from "react"
-import Header from "./components/Header"
-import Game from "./components/Game"
+import React from "react";
+import Header from "./components/Header";
+import Game from "./components/Game";
 
-export default function App(){
+export default function App() {
+    const [todaysSong, setTodaysSong] = React.useState({});
 
-    const [todaysSong, setTodaysSong] = React.useState([])
-
-    // '00s Hits' Playlist ID: 248297032
-    // 'HITS 2023 - Today's Top Songs' Playlist ID: 9890417302
-    // '2010s party hits' Playlist ID: 715215865
-
-    var playlist_ids = [248297032, 9890417302, 715215865]
-    var playlist = playlist_ids[Math.floor(Math.random() * playlist_ids.length)];
-
-    async function getTodaysSong() {
-
-        const options = {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/octet-stream',
-                'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-                'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-            }
-        };
-
-        try {
-            const url = 'https://deezerdevs-deezer.p.rapidapi.com/playlist/'+playlist;
-
-            const response = await fetch(url, options);
-            const result = await response.json();
-            var random_song = result.tracks.data[Math.floor(Math.random() * result.tracks.data.length)];
-
-            if (random_song != null){
-                setTodaysSong(random_song)
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    const songs = [
+        {
+            title: "Song 1",
+            artist: "Artist 1",
+            url: "https://raw.githubusercontent.com/rishabhRsinghvi/Bollywood-Heardle/main/songs/song1.mp3",
+            cover: "https://raw.githubusercontent.com/rishabhRsinghvi/Bollywood-Heardle/main/covers/cover1.jpg"
+        },
+        {
+            title: "Song 2",
+            artist: "Artist 2",
+            url: "https://raw.githubusercontent.com/rishabhRsinghvi/Bollywood-Heardle/main/songs/song2.mp3",
+            cover: "https://raw.githubusercontent.com/rishabhRsinghvi/Bollywood-Heardle/main/covers/cover2.jpg"
+        },
+        // Add more songs as needed
+    ];
 
     React.useEffect(() => {
-        getTodaysSong()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        const randomSong = songs[Math.floor(Math.random() * songs.length)];
+        setTodaysSong(randomSong);
     }, []);
-    
+
     return (
         <div>
             <Header />
             <Game todaysSong={todaysSong} />
             <footer></footer>
         </div>
-    )
+    );
 }
